f6aedf271f722c01b5595e2b4144196916b8bd56
Narek <narek@voxofon.com>
2017-03-30 18:20:10 +0400

    Using ENV variables to set mem quota and index mem quota added default values in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,7 @@ FROM couchbase:community-4.5.0
 
 COPY configure-node.sh /opt/couchbase
 
+ENV MEM_QUOTA 400
+ENV MEM_INDEX_QUOTA 400
+
 CMD ["/opt/couchbase/configure-node.sh"]
--- configure-node.sh
+++ configure-node.sh
@@ -8,7 +8,7 @@ USER="Administrator"
 PASS="password"
 
 # Setup index and memory quota
-curl -v -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=300
+curl -v -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=$MEM_QUOTA -d indexMemoryQuota=$INDEX_MEM_QUOTA
 
 # Setup services
 curl -v http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex
